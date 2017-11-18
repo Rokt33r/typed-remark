1bec56a605d65cee9bbcb43bf6a36ecfcdb1a2ff
Junyoung Choi <fluke8259@gmail.com>
2017-11-18 18:12:43 +0900

    Expose internal types

--- packages/remark-parse/src/lib/index.ts
+++ packages/remark-parse/src/lib/index.ts
@@ -1,5 +1,13 @@
 import { Processor, Attacher } from 'typed-unified'
 import { RemarkParserOptions, parserFactory } from './RemarkParser'
+export {
+  TokenizeMethod,
+  Eat,
+  Apply,
+  Reset,
+  Factory,
+  Tokenize,
+} from './tokenizer'
 
 const RemarkParseAttacher: Attacher<RemarkParserOptions> = function (this: Processor, options: RemarkParserOptions) {
   this.Parser = parserFactory()
